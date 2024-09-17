78267c95bd388297b40df30d124ab6021c907da6
Kyan Cox <kyan.cox@gmail.com>
2024-09-17 15:01:24 -0500

    Navbar updaet

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,16 +3,18 @@
 // components/Navbar.js
 import Link from 'next/link';
 import Image from 'next/image';
+import { useData } from '@/providers/DataContext';
 import { usePathname } from 'next/navigation';
 
 export default function Navbar() {
     const pathname = usePathname();
+    const { setData } = useData()
 
     return (
         <nav className="p-4 mb-2 text-white text-lg font-semibold bg-red-700">
             <div className=" mx-auto flex justify-between items-center font">
                 <div className="">
-                    <Link href="/" className='flex flex-row hover:text-gray-300 transition no-underline'>
+                    <Link href="/" onClick={() => { if (pathname === '/') setData(null); }} className='flex flex-row hover:text-gray-300 transition no-underline'>
                         <p className='my-auto'>DARS Visualizer</p>
                     </Link>
                 </div>
